5754f0c2f697c9d930055cf9bacffe864625ebe1
shijn <306358910@qq.com>
2014-10-25 20:40:53 +0800

    add base permission control

--- app/app.js
+++ app/app.js
@@ -8,19 +8,3 @@ angular.module('myApp', [
   'angular-loading-bar',
   'pasvaz.bindonce'
 ]);
-
-// get userpermissionlist before angular start
-angular.element(document).ready(function() {
-  // $.ajax({
-  //   url: '/api/user/current',
-  //   contentType: "application/json; charset=utf-8",
-  //   type: "GET",
-  //   dataType: 'json'
-  // }).then(function(data){
-  //     for (var i = 0; i < data.permissions.length; i++) {
-  //       data.permissions[i] = data.permissions[i].replace(/\s/g,"");
-  //     };
-  //     userPermissionList = data.permissions;
-  // });
-  angular.bootstrap(document, ['myApp']);
-});
