7d86dcf9ac7446663d6f475a5be66b1ff1229aeb
BigKunLun <306358910@qq.com>
2014-11-19 16:03:29 +0800

    fix ng-app

--- src/app.js
+++ src/app.js
@@ -5,21 +5,3 @@ angular.module('myApp', [
   'myApp.module1',
   'myApp.module2'
 ]);
-
-// (function(document,$,angular){
-//   angular.element(document).ready(function() {
-//     $.ajax({
-//       url: '/api/get_user_permission',
-//       type: "GET",
-//       dataType: 'json'
-//     }).then(function(data){
-//         for (var i = 0; i < data.permissions.length; i++) {
-//           data.permissions[i] = data.permissions[i].replace(/\s/g,"");
-//         };
-//         angular.module('myApp').run(['$rootScope', function($rootScope){
-//           $rootScope = data.permissions;
-//         }]);
-//         angular.bootstrap(document, ['myApp']);
-//     });
-//   });
-// })(document,jQuery,angular);
